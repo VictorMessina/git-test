69d7f42900a96e540947df705b7ee17e74aa8aef
31449530 <31449530@087865-P33S105.macklabs.br>
2014-08-22 08:56:45 -0300

    arquivo foi modificado para o exercico 4.1

--- index/index.php
+++ index/index.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
+        <title> exercicio 4.1</title>
     </head>
     <body>
         <?php
