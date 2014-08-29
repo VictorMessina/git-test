dd85a69520884c043c5aa14a119c280c3ea3e7a9
31449530 <31449530@087865-P33S105.macklabs.br>
2014-08-29 08:46:44 -0300

    exercicios  4.1 , 4.2 e 5.1 finalizados

--- TecWeb/index.php
+++ TecWeb/index.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title> exercicio 4.1 e 4.2</title>
+        <title> exercicio 4.1, 4.2 e 5.1</title>
         <style type="text/css">
 	.estilo1{
 	background-color:gray;
@@ -11,6 +11,7 @@
     </head>
     <body>
 <?php
+// Mostrar se um aluno foi aprovado ou não.
 $P1=9;
 $M1=8;       
 $M2=7;
@@ -54,8 +55,8 @@ echo " <br>
         
         ?>
         
-        <?php 
-
+        <?php
+        // Tabela.
 echo "<table width='0' border='1'> 
    <tr>
     <td><b>ID</td>   
@@ -64,7 +65,20 @@ echo "<table width='0' border='1'>
   </tr>";
                     
   for($i = 1; $i<7; $i = $i + 1){ 
+      $estilo = "estilo1";
+      if ($i%2==0){
+          
     echo "
+     <tr> 
+       <td class = $estilo>id</td>   
+       <td class = $estilo>nome</td> 
+       <td desc class = $estilo>desc</td> 
+    </tr> 
+      ";
+    }
+    
+    else {
+        echo "
      <tr> 
        <td>id</td>   
        <td>nome</td> 
@@ -72,11 +86,11 @@ echo "<table width='0' border='1'>
     </tr> 
       ";
     }
- 
+  }
    ?>
         
      <?php
-        
+        // Mostrar se um numero é primo ou não.
         $Numero=7;
         $Quantidade=0;
         for ($i=1; $i<=$Numero; $i++){
@@ -97,7 +111,8 @@ echo "<table width='0' border='1'>
             <br>"
     ?>
         
-        <?php 
+        <?php
+        // Sequencia Fibonacci
         $x=0; 
         $y=1;
     for ($z=0; $z<9; $z++){
@@ -111,6 +126,49 @@ echo "<table width='0' border='1'>
             <br>"
         ?>
         
+        <?php
         
+        
+        echo "<br><br><br><br>";
+        // tabela com arrays
+            $Turma[0] = "02K01";
+            $Turma[1] = "02J01";
+            $Turma[2] = "02K01";
+
+            $Disiplina[0] = "Tec Web";
+            $Disiplina[1]= "Linguagem de Programação I";
+            $Disiplina[2] = "Linguagem de Programação I";
+
+            $Semana [0] = "Seguenda";
+            $Semana [1] = "Terça";
+            $Semana [2] = "Quarta";
+
+            $Professor [0] = "Charles";
+            $Professor [1] = "João";
+            $Professor [2] = "Charles";
+
+            echo "<table width='0' border='1'> 
+               <tr>
+                <td><b>Turma</td>   
+                <td><b>disiplina</td> 
+                <td><b>Semana</td>
+                <td><b>Professor</td>
+              </tr>";
+
+              for($i = 0; $i<3; $i++){ 
+
+                echo "
+                 <tr> 
+                   <td>$Turma[$i] </td>   
+                   <td>$Disiplina[$i]</td> 
+                   <td>$Semana[$i]</td>
+                   <td> $Professor[$i]</td>
+                </tr> 
+                  ";
+                }
+                        echo "<br><br><br><br>";
+
+
+        ?>
     </body>
 </html>
